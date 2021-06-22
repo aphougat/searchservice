f049b060a92fd4694597b700db4647146b8d5f76
abhayphougat <abhayphougat@gmail.com>
2021-06-22 15:49:34 +0530

    added solr document support

--- src/main/java/com/cosmoneural/search/searchservice/model/ContentModel.java
+++ src/main/java/com/cosmoneural/search/searchservice/model/ContentModel.java
@@ -2,19 +2,32 @@ package com.cosmoneural.search.searchservice.model;
 
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.solr.core.mapping.Indexed;
+import org.springframework.data.solr.core.mapping.SolrDocument;
 
 import java.util.List;
 
 @Data
+@SolrDocument(collection = "content")
 public class ContentModel {
+    @Id
+    @Indexed(name = "id", type = "string")
     String id;
+    @Indexed(name = "title", type = "text")
     String title;
+    @Indexed(name = "desc", type = "text")
     String desc;
+    @Indexed(name = "contentUrl", type = "string")
     String contentUrl;
+    @Indexed(name = "mediaUrl", type = "string")
     String mediaUrl;
+    @Indexed(name = "name", type = "text")
     List<String> keywords;
-    UserModel user;
-    String contentText;
+    @Indexed(name = "user", type = "string")
+    String user;
+    @Indexed(name = "productId", type = "text")
     List<String> productId;
+    @Indexed(name = "productNames", type = "text")
     List<String> productNames;
 }
--- src/main/java/com/cosmoneural/search/searchservice/model/UserModel.java
+++ src/main/java/com/cosmoneural/search/searchservice/model/UserModel.java
@@ -1,16 +1,31 @@
 package com.cosmoneural.search.searchservice.model;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.solr.core.mapping.Indexed;
+import org.springframework.data.solr.core.mapping.SolrDocument;
+
+import java.util.List;
 
 @Data
+@SolrDocument(collection = "user")
 public class UserModel {
 
-    String id;
-    String name;
+    @Id
+    @Indexed(name = "id", type = "string")
+    private String id;
+
+    @Indexed(name = "name", type = "string")
+    private String name;
+    @Indexed(name = "city", type = "string")
     String city;
+    @Indexed(name = "state", type = "string")
     String state;
+    @Indexed(name = "brand", type = "string")
     boolean brand;
+    @Indexed(name = "influencer", type = "boolean")
     boolean influencer;
-    String keywords;
+    @Indexed(name = "keywords", type = "text")
+    List<String> keywords;
 
 }
