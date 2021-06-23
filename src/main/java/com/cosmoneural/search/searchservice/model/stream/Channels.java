cd1d6220b781901b5d1972da4f90e2a3a2bc5aec
abhayphougat <abhayphougat@gmail.com>
2021-06-23 18:49:58 +0530

    moved to streams 3.1.1 -- need to test connectivity to kafka

--- src/main/java/com/cosmoneural/search/searchservice/model/stream/Channels.java
+++ src/main/java/com/cosmoneural/search/searchservice/model/stream/Channels.java
@@ -1,14 +1,18 @@
 package com.cosmoneural.search.searchservice.model.stream;
 
 import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.MessageChannel;
+
+import java.util.function.Consumer;
 
 public interface Channels {
 
-    @Input
+
+    @Bean
     MessageChannel content();
 
-    @Input
+    @Bean
     MessageChannel user();
 
 }
